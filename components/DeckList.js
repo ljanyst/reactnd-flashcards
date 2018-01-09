a37d2425dbe52b87f8eb5902227cacb5cde511fe
Lukasz Janyst <lukasz@jany.st>
2018-01-09 19:34:03 +0100

    Add stubs of DeckList

---
+++ components/DeckList.js
@@ -0,0 +1,22 @@
+//------------------------------------------------------------------------------
+// Author: Lukasz Janyst <lukasz@jany.st>
+// Date: 08.01.2018
+//------------------------------------------------------------------------------
+
+import React, { Component } from 'react';
+import { View, Text} from 'react-native';
+
+//------------------------------------------------------------------------------
+// Deck List
+//------------------------------------------------------------------------------
+class DeckList extends Component {
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <Text>DeckList</Text>
+      </View>
+    );
+  }
+}
+
+export default DeckList;
