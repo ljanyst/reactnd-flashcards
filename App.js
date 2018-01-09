5230a2608b69f0facc1e07d76b5945c98a3e548e
Lukasz Janyst <lukasz@jany.st>
2018-01-09 19:35:25 +0100

    Add stack navigator to the app

--- App.js
+++ App.js
@@ -1,23 +1,93 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+//------------------------------------------------------------------------------
+// Author: Lukasz Janyst <lukasz@jany.st>
+// Date: 08.01.2018
+//------------------------------------------------------------------------------
 
-export default class App extends React.Component {
+import React, { Component } from 'react';
+import {
+  View, StatusBar, Platform, StyleSheet
+} from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import { Constants } from 'expo';
+import { MaterialIcons, Ionicons } from '@expo/vector-icons';
+
+import { coral, white } from './utils/colors';
+
+import DeckList from './components/DeckList';
+import Touchable from './components/Touchable';
+
+//------------------------------------------------------------------------------
+// Stylesheet
+//------------------------------------------------------------------------------
+const navBarFgColor = Platform.OS === 'ios' ? coral : white;
+const navBarBgColor = Platform.OS === 'ios' ? white : coral;
+const styles = StyleSheet.create({
+  navBarBtn: {
+    color: navBarFgColor
+  },
+  navBarBtnContainer: {
+    marginRight: 12
+  }
+});
+
+//------------------------------------------------------------------------------
+// Status Bar
+//------------------------------------------------------------------------------
+function FlashCardsStatusBar( {backgroundColor, ...props} ) {
+  return (
+    <View style={{backgroundColor, height: Constants.statusBarHeight}}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  );
+}
+
+//------------------------------------------------------------------------------
+// Add deck button
+//------------------------------------------------------------------------------
+const addDeckBtn = (
+  <View style={styles.navBarBtnContainer}>
+    <Touchable onPress={() => alert('!!!')}>
+      {
+        Platform.OS === 'ios'
+          ? <Ionicons name='ios-add' size={32} style={styles.navBarBtn}/>
+          : <MaterialIcons name='add' size={32} style={styles.navBarBtn}/>
+      }
+    </Touchable>
+  </View>
+);
+
+//------------------------------------------------------------------------------
+// Main navigator
+//------------------------------------------------------------------------------
+const MainNavigator = StackNavigator({
+  DeckList: {
+    screen: DeckList,
+    navigationOptions: ({navigation}) => ({
+      title: 'Your decks',
+      headerRight: addDeckBtn
+    })
+  }
+}, {
+  navigationOptions: ({navigation}) => ({
+    headerTintColor: navBarFgColor,
+    headerStyle: {
+      backgroundColor: navBarBgColor
+    }
+  })
+});
+
+//------------------------------------------------------------------------------
+// The main component
+//------------------------------------------------------------------------------
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{flex: 1}}>
+        <FlashCardsStatusBar
+          backgroundColor={coral}
+          barStyle='light-content'/>
+        <MainNavigator/>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
