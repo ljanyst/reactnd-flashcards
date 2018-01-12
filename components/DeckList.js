da79a901c5dea0464fc48f2198341ac6b324837a
Lukasz Janyst <lukasz@jany.st>
2018-01-12 17:30:14 +0100

    Implement DeckList

--- components/DeckList.js
+++ components/DeckList.js
@@ -4,7 +4,59 @@
 //------------------------------------------------------------------------------
 
 import React, { Component } from 'react';
-import { View, Text} from 'react-native';
+import { View, Text, FlatList, StyleSheet, Platform } from 'react-native';
+import { connect } from 'react-redux';
+
+import { white, coral, gray } from '../utils/styles';
+
+import Touchable from './Touchable';
+
+//------------------------------------------------------------------------------
+// Styles
+//------------------------------------------------------------------------------
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: white,
+    borderRadius: Platform.OS === 'ios' ? 10 : 2,
+    padding: 20,
+    marginLeft: 15,
+    marginRight: 15,
+    marginTop: 17,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: 'rgba(0,0,0,0.24)',
+    shadowOffset: {
+      width: 0,
+      height: 1
+    },
+    elevation: 1
+  },
+
+  title: {
+    color: coral,
+    fontSize: 22
+  },
+
+  cardCounter: {
+    marginTop: 15,
+    color: gray,
+    fontSize: 14
+  }
+});
+
+//------------------------------------------------------------------------------
+// Render item
+//------------------------------------------------------------------------------
+function itemView({item}) {
+  return (
+    <Touchable  style={styles.item}>
+      <Text style={styles.title}>{item.title}</Text>
+      <Text style={styles.cardCounter}>Cards: {item.numCards}</Text>
+    </Touchable>
+  );
+}
 
 //------------------------------------------------------------------------------
 // Deck List
@@ -12,11 +64,31 @@ import { View, Text} from 'react-native';
 class DeckList extends Component {
   render() {
     return (
-      <View style={{flex: 1}}>
-        <Text>DeckList</Text>
-      </View>
+      <FlatList
+        data={this.props.deckList}
+        renderItem={itemView}
+      />
     );
   }
 }
 
-export default DeckList;
+//------------------------------------------------------------------------------
+// Connect redux
+//------------------------------------------------------------------------------
+function mapStateToProps(state) {
+  return {
+    deckList: Object.keys(state).map(deckId => {
+      return {
+        key: deckId,
+        title: deckId,
+        numCards: state[deckId].questions.length
+      };
+    })
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DeckList);
