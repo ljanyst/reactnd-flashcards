d8055d596ec958109baeb2dd1f96a296995594ce
Lukasz Janyst <lukasz@jany.st>
2018-01-12 01:11:57 +0100

    Create button styles

--- utils/styles.js
+++ utils/styles.js
@@ -3,5 +3,43 @@
 // Date: 08.01.2018
 //------------------------------------------------------------------------------
 
+import { StyleSheet, Platform } from 'react-native';
+
+//------------------------------------------------------------------------------
+// Colors
+//------------------------------------------------------------------------------
 export const coral='#ff7f50';
 export const white = '#fff';
+
+//------------------------------------------------------------------------------
+// Button Styles
+//------------------------------------------------------------------------------
+const iosButton = {
+  backgroundColor: coral,
+  padding: 10,
+  borderRadius: 7,
+  height: 45,
+  marginLeft: 40,
+  marginRight: 40
+};
+
+const androidButton = {
+  backgroundColor: coral,
+  padding: 10,
+  paddingLeft: 30,
+  paddingRight: 30,
+  height: 45,
+  borderRadius: 2,
+  alignSelf: 'flex-end',
+  justifyContent: 'center',
+  alignItems: 'center'
+};
+
+export const buttonStyles = StyleSheet.create({
+  button: Platform.OS === 'ios' ? iosButton : androidButton,
+  text: {
+    color: white,
+    fontSize: 22,
+    textAlign: 'center'
+  }
+});
