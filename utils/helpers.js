f12883e016936b3d4533fc1899fc6522b6a48e8b
Lukasz Janyst <lukasz@jany.st>
2018-01-14 16:25:59 +0100

    Implement makeId

---
+++ utils/helpers.js
@@ -0,0 +1,17 @@
+//------------------------------------------------------------------------------
+// Author: Lukasz Janyst <lukasz@jany.st>
+// Date: 14.01.2018
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Make id
+//------------------------------------------------------------------------------
+export function makeId(length) {
+  var text = '';
+  var possible = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+  for(var i = 0; i < length; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
