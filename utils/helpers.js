2d626c67f81fb08e00ddefdc0c5d151a846da1e1
Lukasz Janyst <lukasz@jany.st>
2018-01-16 21:55:44 +0100

    Implement array shuffling

--- utils/helpers.js
+++ utils/helpers.js
@@ -15,3 +15,16 @@ export function makeId(length) {
 
   return text;
 }
+
+//------------------------------------------------------------------------------
+// Fisher-Yates Shuffle
+//------------------------------------------------------------------------------
+export function shuffle(array) {
+  for(let i = array.length-1; i >= 0; --i) {
+    let j = Math.floor(Math.random()*(i+1));
+    let tmp = array[i];
+    array[i] = array[j];
+    array[j] = tmp;
+  }
+  return array;
+}
